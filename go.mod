9804007e12888d481046cf79ba35c5adf306bae1
Wim <wim@42.be>
2019-05-25 23:45:08 +0200

    Update to go modules. Update dependencies

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/42wim/ipsetd
+
+go 1.12
+
+require (
+	github.com/firstrow/tcp_server v0.0.0-20190424084220-b7a05ff2879d
+	github.com/kr/pty v1.1.4
+)
--- vendor/github.com/firstrow/tcp_server/tcp_server.go
+++ vendor/github.com/firstrow/tcp_server/tcp_server.go
@@ -2,6 +2,7 @@ package tcp_server
 
 import (
 	"bufio"
+	"crypto/tls"
 	"log"
 	"net"
 )
@@ -14,8 +15,8 @@ type Client struct {
 
 // TCP server
 type server struct {
-	clients                  []*Client
 	address                  string // Address to open connection: localhost:9999
+	config                   *tls.Config
 	onNewClientCallback      func(c *Client)
 	onClientConnectionClosed func(c *Client, err error)
 	onNewMessage             func(c *Client, message string)
@@ -23,6 +24,7 @@ type server struct {
 
 // Read client data from channel
 func (c *Client) listen() {
+	c.Server.onNewClientCallback(c)
 	reader := bufio.NewReader(c.conn)
 	for {
 		message, err := reader.ReadString('\n')
@@ -70,9 +72,15 @@ func (s *server) OnNewMessage(callback func(c *Client, message string)) {
 	s.onNewMessage = callback
 }
 
-// Start network server
+// Listen starts network server
 func (s *server) Listen() {
-	listener, err := net.Listen("tcp", s.address)
+	var listener net.Listener
+	var err error
+	if s.config == nil {
+		listener, err = net.Listen("tcp", s.address)
+	} else {
+		listener, err = tls.Listen("tcp", s.address, s.config)
+	}
 	if err != nil {
 		log.Fatal("Error starting TCP server.")
 	}
@@ -85,7 +93,6 @@ func (s *server) Listen() {
 			Server: s,
 		}
 		go client.listen()
-		s.onNewClientCallback(client)
 	}
 }
 
@@ -94,6 +101,25 @@ func New(address string) *server {
 	log.Println("Creating server with address", address)
 	server := &server{
 		address: address,
+		config:  nil,
+	}
+
+	server.OnNewClient(func(c *Client) {})
+	server.OnNewMessage(func(c *Client, message string) {})
+	server.OnClientConnectionClosed(func(c *Client, err error) {})
+
+	return server
+}
+
+func NewWithTLS(address string, certFile string, keyFile string) *server {
+	log.Println("Creating server with address", address)
+	cert, _ := tls.LoadX509KeyPair(certFile, keyFile)
+	config := tls.Config{
+		Certificates: []tls.Certificate{cert},
+	}
+	server := &server{
+		address: address,
+		config:  &config,
 	}
 
 	server.OnNewClient(func(c *Client) {})
